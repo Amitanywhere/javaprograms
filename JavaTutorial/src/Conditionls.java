fca884eac77c11302ffaadd9c4ed689e145ab15a
Amitanywhere <amit.anup@anywhere.co>
2023-01-09 10:16:24 +0530

    Some basic java program

--- JavaTutorial/src/Conditionls.java
+++ JavaTutorial/src/Conditionls.java
@@ -2,32 +2,61 @@ import java.util.Scanner;
 
 public class Conditionls{
     public static void main(String[] args){
-        byte age = 10;
-        if(age >= 18){
-            System.out.println("You can enter");
-        }
-        else {
-            System.out.println("You can not enter");
-        }
+//        byte age = 10;
+//        if(age >= 18){
+//            System.out.println("You can enter");
+//        }
+//        else {
+//            System.out.println("You can not enter");
+//        }
 
         Scanner scn = new Scanner(System.in);
-        System.out.println("Enter Age");
-        int iage = scn.nextInt();
-        switch (iage){
-            case 18:
-                System.out.println("You are become just adult");
-                break;
-            case 30:
-                System.out.println("You are already a adult");
-                break;
-            case 15:
-                System.out.println("You are not an adult");
-                break;
-            default:
-                System.out.println("Your age " + iage);
-                break;
+//        System.out.println("Enter Age");
+//        int iage = scn.nextInt();
+//        switch (iage){
+//            case 18:
+//                System.out.println("You are become just adult");
+//                break;
+//            case 30:
+//                System.out.println("You are already a adult");
+//                break;
+//            case 15:
+//                System.out.println("You are not an adult");
+//                break;
+//            default:
+//                System.out.println("Your age " + iage);
+//                break;
+//        }
+
+        // tax calculate <250000 tax = 0 , >=250000 to <= 500000 tax = 5% , >500000 to <=1000000 tax = 10% , >1000000 tax = 20%
+//        System.out.println("Enter Your Income :");
+//        int income = scn.nextInt();
+//        float tax = 0.00f;
+//        if(income >= 250000 && income <= 500000){
+//            tax += income * 0.05f;
+//        }
+//        else if(income > 500000 && income <= 1000000){
+//            tax += 500000 * 0.05f + (income - 500000) * 0.1f;
+//        }
+//        else if(income > 1000000){
+//            tax += 500000 * 0.05f + 500000 * 0.1f + (income-1000000) * 0.2f;
+//        }
+//
+//        System.out.println("You have to pay tax amount Rs. :" + tax);
+        
+        // Type of website
+        System.out.println("Enter website");
+        String website = scn.nextLine();
+        if(website.endsWith(".com")){
+            System.out.println("Commercial website");
+        } else if (website.endsWith(".org")) {
+            System.out.println("Organization website");
+        } else if (website.endsWith(".in")) {
+            System.out.println("Indian website");
         }
-        // Switch case
+        else {
+            System.out.println("Other website");
+        }
     }
 
 
---
+++ JavaTutorial/src/Loops.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args){
+        // star pattern printing
+//        for(int i=0; i<5; i++){
+//            for(int j=5-i; j>0; j--){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        // sum of first n even numbers
+//        int sum = 0;
+//        int i = 0 , n = 10;
+//        while(i < n){
+//            sum += 2 * i;
+//            i++;
+//        }
+//        System.out.println("Sum of first 10 even numbers: " + sum);
+
+        // multiplication table of a number
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Number for printing table: ");
+        int number = scn.nextInt();
+//        int i = 1;
+//        while(i <= 10){
+//            System.out.printf("%d x %d = %d\n" , number , i , number * i);
+//            i++;
+//        }
+
+        // factorial
+        int factorial = 1;
+        while(number > 1){
+            factorial *= number;
+            number--;
+        }
+        System.out.println("Factorial is: " + factorial);
+    }
+}
